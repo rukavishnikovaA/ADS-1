2deee4f8b8691e98f73d56df5b6d4d56c1dc6960
Arina Rukavishnikova <75798616+rukavishnikovaA@users.noreply.github.com>
2021-03-22 18:51:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,24 @@
 // Copyright 2021 NNTU-CS
-
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int start = 0;
+    int end = size - 1;
+    int count = 0;
+    
+    while (start < end) {
+        int k = (start + end) / 2;
+        if (arr[k] < value) {
+            start = k+1;
+        } else {
+            end = k;
+        }
+    }
+
+    if (arr[start] == value) {
+        while (arr[start] == value) {
+            count++;
+            start++;
+        }
+    }
+    
+    return count;
 }
