3f151fe101697eb98380f7f3957d6aad30c2886b
Arina Rukavishnikova <75798616+rukavishnikovaA@users.noreply.github.com>
2021-03-22 18:58:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@ int cbinsearch(int *arr, int size, int value) {
     int start = 0;
     int end = size - 1;
     int count = 0;
-    
+
     while (start < end) {
         int k = (start + end) / 2;
         if (arr[k] < value) {
@@ -19,6 +19,6 @@ int cbinsearch(int *arr, int size, int value) {
             start++;
         }
     }
-    
+
     return count;
 }
